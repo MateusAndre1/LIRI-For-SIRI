c9b053efbdddeacedf4395327fe5de3eeef6d64f
Andre Roque <mateusandre1@yahoo.com>
2019-09-28 11:35:18 -0400

    finished conertThis

--- liri.js
+++ liri.js
@@ -5,36 +5,69 @@ var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
 var fs = require("fs");
 var moment = require("moment");
-console.log(keys.spotify);
+// console.log(keys.spotify);
 
 
 var whatToDo = process.argv[2];
-var userInput = process.argv[3];
+var userInput = process.argv.slice(3).join("+");
+var userInput2 = process.argv.slice(3).join(" ");
 
-function spotifyThis(input){
+function spotifyThis(input) {
 
     spotify
-    .search({ type: 'track', query: input, limit: 1 })
-    .then(function(response) {
-      console.log(JSON.stringify(response));
-    })
-    .catch(function(err) {
-      console.log(err);
-    });
+        .search({
+            type: 'track',
+            query: input,
+            limit: 1
+        })
+        .then(function (response) {
+            console.log("\n=====================================\n");
+            console.log(JSON.stringify(response.tracks.items[0].artists[0].name));
+            console.log(JSON.stringify(response.tracks.items[0].name));
+            console.log(JSON.stringify(response.tracks));
+            console.log("\n=====================================\n");
+            
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 
 
 
 }
 
-function concertThis(){
+function concertThis(input) {
 
+    var queryUrl = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp";    
+
+    axios.get(queryUrl).then(
+       function (response){
+
+            let venueName = response.data[0].venue.name;
+            let venueLocation = response.data[0].venue.city + ", " + response.data[0].venue.region;
+            let date = moment(response.data[0].datetime).format("MM/DD/YYYY");           
+            console.log("\n=====================================\n");
+            console.log(`\nCurrent concert information for: ${userInput2}\nConcert Venue: ${venueName}\nVenue Location: ${venueLocation}\nConcert Date: ${date}`);
+            console.log("\n=====================================\n");
+        })
+        .catch(err => {
+            console.log(err);
+        });
 }
 
-function movieThis(){
+function movieThis(input) {
 
+    var queryUrl = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy";
+    axios.get(queryUrl).then(
+        function(response) {
+          console.log("Release Year: " + response.data.Year);
+        })
+        .catch(err => {
+            console.log(err);
+        });
 }
 
-function doWhatItSays(){
+function doWhatItSays() {
 
 }
 
