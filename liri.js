ba85f70410f5b7d7916e022227fac8b038e4f76a
Andre Roque <mateusandre1@yahoo.com>
2019-09-28 12:17:11 -0400

    added information for ombd call

--- liri.js
+++ liri.js
@@ -7,10 +7,13 @@ var fs = require("fs");
 var moment = require("moment");
 // console.log(keys.spotify);
 
+// global variables for arguments in node
 
 var whatToDo = process.argv[2];
 var userInput = process.argv.slice(3).join("+");
 var userInput2 = process.argv.slice(3).join(" ");
+
+// call spotify api to get songs information
 
 function spotifyThis(input) {
 
@@ -20,53 +23,65 @@ function spotifyThis(input) {
             query: input,
             limit: 5
         })
-        .then(function (response) {
+        .then(
+            (response) => {
 
-            for (let i = 0; i < response.tracks.items.length; i++) {
-                        
-            let artist = response.tracks.items[i].artists[0].name;
-            let songName = response.tracks.items[i].name;
-            let previewLink = response.tracks.items[i].external_urls.spotify;
-            let albumName = response.tracks.items[i].album.name;
-            console.log("\n=====================================\n");
-            console.log(`\nArtist: ${artist}\nSong: ${songName}\nAlbum: ${albumName}\nLink to song: ${previewLink}`);
-            console.log("\n=====================================\n");
-        }
-        })
+                for (let i = 0; i < response.tracks.items.length; i++) {
+
+                    let artist = response.tracks.items[i].artists[0].name;
+                    let songName = response.tracks.items[i].name;
+                    let previewLink = response.tracks.items[i].external_urls.spotify;
+                    let albumName = response.tracks.items[i].album.name;
+                    console.log("\n=====================================\n");
+                    console.log(`\nArtist: ${artist}\nSong: ${songName}\nAlbum: ${albumName}\nLink to song: ${previewLink}`);
+                    console.log("\n=====================================\n");
+                }
+            })
         .catch(function (err) {
             console.log(err);
         });
-
-
-
 }
+
+// call bandsintown api for concert information
 
 function concertThis(input) {
 
-    var queryUrl = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp";    
+    var queryUrl = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp";
 
     axios.get(queryUrl).then(
-       function (response){
+            (response) => {
 
-            let venueName = response.data[0].venue.name;
-            let venueLocation = response.data[0].venue.city + ", " + response.data[0].venue.region;
-            let date = moment(response.data[0].datetime).format("MM/DD/YYYY");           
-            console.log("\n=====================================\n");
-            console.log(`\nCurrent concert information for: ${userInput2}\nConcert Venue: ${venueName}\nVenue Location: ${venueLocation}\nConcert Date: ${date}`);
-            console.log("\n=====================================\n");
-        })
+                let venueName = response.data[0].venue.name;
+                let venueLocation = response.data[0].venue.city + ", " + response.data[0].venue.region;
+                let date = moment(response.data[0].datetime).format("MM/DD/YYYY");
+                console.log("\n=====================================\n");
+                console.log(`\nCurrent Concert Information For: ${userInput2}\nConcert Venue: ${venueName}\nVenue Location: ${venueLocation}\nConcert Date: ${date}`);
+                console.log("\n=====================================\n");
+            })
         .catch(err => {
             console.log(err);
         });
 }
 
+// use axios to make a request from ombd server to get movie information
+
 function movieThis(input) {
 
     var queryUrl = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy";
     axios.get(queryUrl).then(
-        function(response) {
-          console.log("Release Year: " + response.data.Year);
-        })
+            (response) => {
+                let title = response.data.Title;
+                let yearReleased = response.data.Year;
+                let rating = response.data.imdbRating;
+                let rottenTom = response.data.Ratings[1].Value;
+                let language = response.data.Language;
+                let plot = response.data.Plot;
+                let countryProduced = response.data.Country;
+                let actors = response.data.Actors;
+                console.log("\n=====================================\n");
+                console.log(`\nTitle: ${title}\n Year Released: ${yearReleased}\nIMBD Rating: ${rating}\nRotten Tomatoes Rating: ${rottenTom}\nCountry Produced In: ${countryProduced}\nLanguage: ${language}\nPlot: ${plot}\nActors: ${actors}`);
+                console.log("\n=====================================\n");
+            })
         .catch(err => {
             console.log(err);
         });
@@ -75,6 +90,8 @@ function movieThis(input) {
 function doWhatItSays() {
 
 }
+
+// give different cases for which function to call up
 
 switch (whatToDo) {
     case "spotify-this-song":
